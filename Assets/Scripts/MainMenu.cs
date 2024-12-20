ccd13e9f4afb479b24894f20cd530c3635a47973
smonz08 <sara.mongno@gmail.com>
2024-12-20 01:39:21 -0500

    Timer Updates

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-       SceneManager.LoadSceneAsync(1);
+       SceneManager.LoadSceneAsync(0);
     }
 
     public void QuitGame()
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,6 +9,7 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    public GameObject pauseMenu;
 
     // Update is called once per frame
     void Update()
@@ -23,5 +24,17 @@ public class Timer : MonoBehaviour
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
